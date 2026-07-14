329c3b60bfa7e12c427b71b5f501e34a9e60d58f
Milos Djukic <132833088+grifon95@users.noreply.github.com>
2026-07-14 14:02:12 +0200

    Update stips.py

--- stips.py
+++ stips.py
@@ -1,9 +1,19 @@
+import requests
+
+
 def uzmi_cene():
 
-    cene = {
-        "psenica": 19.65,
-        "kukuruz": 20.02,
-        "soja": 53.00
-    }
+    url = "https://www.stips.minpolj.gov.rs/srl/vest/promet-robe-na-produktnoj-berzi-od-6-do-10-jula-2026-godine"
 
-    return cene
+    response = requests.get(url)
+
+    print("STIPS STATUS:", response.status_code)
+
+    tekst = response.text.lower()
+
+
+    return {
+        "psenica": "pronadjena stranica",
+        "kukuruz": len(tekst),
+        "soja": "ok"
+    }
