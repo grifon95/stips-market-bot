0412998693469686db151e8776edbaa48191437a
Milos Djukic <132833088+grifon95@users.noreply.github.com>
2026-07-14 14:29:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
+import requests
 from datetime import datetime
 
 from stips import uzmi_cene
 from istorija import sacuvaj_cene
 from analiza import napravi_analizu
+
 
 print("🚀 STIPS BOT START")
 
@@ -13,19 +16,31 @@ datum = datetime.now().strftime("%d.%m.%Y")
 # uzimamo cene sa STIPS-a
 cene = uzmi_cene()
 
+print("IZVUČENO:")
+print(cene)
 
-# čuvamo u istoriju
+
+# čuvamo istoriju
 istorija = sacuvaj_cene(cene)
-analiza = napravi_analizu()
 
-print(analiza)
 
 print("\nISTORIJA:")
 print(istorija)
 
 
+# pravimo analizu
+analiza = napravi_analizu()
 
-poruka = f"""
+
+print("\nANALIZA:")
+print(analiza)
+
+
+
+# ako nema dovoljno podataka, ipak šaljemo cenu
+if "Nema dovoljno podataka" in analiza:
+
+    poruka = f"""
 📊 STIPS MARKET ALERT
 
 Datum: {datum}
@@ -38,8 +53,45 @@ Datum: {datum}
 
 🫘 Soja:
 {cene['soja']} din/kg
+
+
+⚠️ Još nema dovoljno istorije za trend analizu.
+"""
+
+else:
+
+    poruka = f"""
+📊 STIPS MARKET ALERT
+
+Datum: {datum}
+
+
+{analiza}
 """
 
 
 print("\nPORUKA:")
 print(poruka)
+
+
+
+# TELEGRAM
+
+token = os.environ["BOT_TOKEN"]
+chat_id = os.environ["CHAT_ID"]
+
+
+url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+
+response = requests.post(
+    url,
+    data={
+        "chat_id": chat_id,
+        "text": poruka
+    }
+)
+
+
+print("\nTelegram:")
+print(response.text)
