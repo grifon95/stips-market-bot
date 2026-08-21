bf2963aefa30eb3d4582f1edebc27ef32bc355e5
Milos Djukic <132833088+grifon95@users.noreply.github.com>
2026-08-21 18:07:56 +0200

    Update test_urea.py

--- test_urea.py
+++ test_urea.py
@@ -2,5 +2,13 @@ from urea import uzmi_ureu
 
 podaci = uzmi_ureu()
 
-print("\nUREA TEST:")
-print(podaci)
+print("\n====================")
+print("UREA TEST")
+print("====================")
+
+print("Cena din/kg:", podaci["cena_din"])
+print("Cena EUR/t:", podaci["cena_eur"])
+print("Pakovanje:", podaci["pakovanje"])
+print("Period:", podaci["period"])
+print("Izveštaj:", podaci["izvestaj"])
+print("Link:", podaci["link"])
